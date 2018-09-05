2762ef38e88493028635b0a806a7b4e906cc3e64
Alinnimp <linnimonteirop@gmail.com>
2018-09-05 13:35:49 -0300

    versao final

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 function cardValidator(numCartao) {
-  if (!numCartao) {
-    throw new Error("Não pode ser vazio");
-  }
-  if (typeof numCartao === "string") {
-    throw new Error("Não pode conter letras");
-  }
-  if (numCartao.toString().length < 10) {
-    throw new Error("Tem que haver mais que 1 digito")
-  }
+    if (!numCartao) {
+        throw new Error("Não pode ser vazio");
+    }
+    if (typeof numCartao === "string") {
+        throw new Error("Contém string");
+    }
+    if (numCartao.toString().length < 10) {
+        throw new Error("Digitos menor do que o esperado");
+    }
 
-  let cardNum = numCartao.toString().split("").reverse();
-  let sumTotal = 0;
+    let numCard = numCartao.toString().split("").reverse();
+    let total = 0;
 
-  for (let i = 0; i < cardNum.length; i++) {
-    if (i % 2 === 1){
-      let quantNum = parseInt(cardNum[i]) * 2;
-      if (quantNum >= 10) {
-        sumTotal += parseInt(quantNum / 10) + (quantNum % 10);
-      }
+    for (let i = 0; i < numCard.length; i++) {
+        if (i % 2 === 1) {
+            let quantNum = parseInt(numCard[i]) * 2;
+            if (quantNum >= 10) {
+                total += parseInt(quantNum / 10) + (quantNum % 10);
+            }
+        } else {
+            total += parseInt(numCard[i]);
+        }
+    }
+    if (total % 10 === 0) {
+        return true;
     } else {
-        sumTotal += parseInt(cardNum[i]);
-      }
-  }
-
-  if (sumTotal % 10 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+        return false;
+    }
 }
 module.exports.cardValidator = cardValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,35 +2,39 @@ const chai = require('chai');
 const expect = chai.expect;
 const validator = require('../index');
 
-describe('validator', function() {
-  describe('#cardValidator()', function() {
-    describe('Quando nao houver parametro deve lancar um erro.', function(){
-      it('Deve lançar um erro', function() {
-        let badFn = function () { validator.cardValidator(); };
-        expect(badFn).to.throw(Error);
-      });
-    });
-    describe('Quando o numero for uma string deve lancar um erro', function(){
-      it('Deve lançar um erro', function() {
-        let badFn = function () { validator.cardValidator(); };
-        expect(badFn).to.throw(Error);
-      });
-    });
-    describe('Quando o numero for um inteiro e houver um digito deve lancar um erro', function(){
-      it('Deve lançar um erro', function() {
-        let badFn = function () { validator.cardValidator(); };
-        expect(badFn).to.throw(Error);
-      });
-    });
-    describe('Quando o numero for um inteiro e for um cartão válido deve retornar true.', function(){
-      it('Deve lançar um erro', function() {
-        expect(validator.cardValidator(5555666677778884)).to.be.true;
-      });
-    });
-    describe('Se o cartao for invalido deve retornar false.', function(){
-      it('Deve lançar um erro', function() {
-        expect(validator.cardValidator(5390830298398279)).to.be.false;
-      });
-    });
-  });
+describe('card', function () {
+ describe('#cardValidator', function () {
+   describe('Quando não houver parametro.', function () {
+     it('Deve mostrar um erro.', function () {
+       let badFn = function () { validator.cardValidator(); };
+       expect(badFn).to.throw(Error);
+     });
+   });
+
+   describe('Quando for uma string.', function () {
+     it('Deve mostrar um erro.', function () {
+       let badFn = function () { validator.cardValidator('Teste'); };
+       expect(badFn).to.throw(Error);
+     });
+   });
+
+   describe('Quando o numero for inteiro e houver apenas um digito.', function () {
+     it('Deve mostrar um erro..', function () {
+       let badFn = function () { validator.cardValidator(12345); };
+       expect(badFn).to.throw(Error);
+     });
+   });
+
+   describe('Quando o numero for válido deve retornar true.', function () {
+     it('Deve mostrar um erro..', function () {
+       expect(validator.cardValidator(5555666677778884)).to.be.true;
+     });
+   });
+
+   describe('Se o cartao for invalido deve retornar false.', function () {
+     it('Deve mostrar um erro.', function () {
+       expect(validator.cardValidator(5390830298398279)).to.be.false;
+     });
+   });
+ });
 });
